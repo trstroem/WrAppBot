d5a641db288ee21574345cddda85c661a094ae9b
trstroem <trstroem@microsoft.com>
2019-07-17 17:33:28 -0700

    new AI version

--- WrAppBot/Dialogs/StoryDetailsDialog.cs
+++ WrAppBot/Dialogs/StoryDetailsDialog.cs
@@ -18,6 +18,19 @@ using System.Runtime.Serialization.Json;
 using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.Bot.Builder.Adapters;
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Rest;
+using System.Collections;
+using System.Diagnostics.Tracing;
 
 namespace WrAppBot.Dialogs
 {
@@ -85,16 +98,31 @@ namespace WrAppBot.Dialogs
             var storyDetails = (StoryDetails)stepContext.Options;
             storyDetails.StoryLine = (string)stepContext.Result;
 
-            //Code for making call to cognitive service
+            var resultCS = new CognitiveResult();
 
-            var resultText = CallCognitiveWebService(storyDetails.StoryLine);
+            var resultCSString = await CallCognitiveWebServiceAsync(storyDetails.StoryLine);
+
+            var deserializedDocument = JsonConvert.DeserializeObject<CognitiveResult>(resultCSString);
 
             //End code for call to cognitive service
 
-            var msg = $"Result from AI call: {resultText} ";
+            //Now, we need to disentangle the result, which looks like this:
+            // {“documents”:[{“id”:“1”,“keyPhrases”:[“gnome”,“pink clothes”,“bitcoin wallet”]}],“errors”:[]}
+
+ 
+            // Getting the first keyword
+            var firstKeyword = deserializedDocument.KeyPhrases[0].keyword.ToString();
+            var sentiment = deserializedDocument.Sentiment;
+
+            var resultFromStoryAI = callStoryAI(firstKeyword, sentiment);
+
+            var msg = $"{resultFromStoryAI}";
+
+
 
             return await stepContext.PromptAsync(nameof(ConfirmPrompt), new PromptOptions { Prompt = MessageFactory.Text(msg) }, cancellationToken);
         }
+
 
         private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
         {
@@ -125,43 +153,60 @@ namespace WrAppBot.Dialogs
         }
 
 
-        private string CallCognitiveWebService(string documentText)
+        private async Task<string> CallCognitiveWebServiceAsync(string documentText)
         {
 
-            string url = "https://westeurope.api.cognitive.microsoft.com/text/analytics/v2.1/keyPhrases";
-            string subscriptionKeyValue = "ba133c233f2849ea9f904b15903fda74";
-            const string SUBSCRIPTION_KEY_NAME = "Ocp-Apim-Subscription-Key";
-            string result;
+            //New
+            string SubscriptionKey = "ba133c233f2849ea9f904b15903fda74";
+            string Endpoint = "https://westeurope.api.cognitive.microsoft.com";
 
-            //create the request for the URL
-            var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-            httpWebRequest.ContentType = "application/json";
-            httpWebRequest.Method = "POST";
 
-            //append subscirption as header
-            httpWebRequest.Headers.Add(SUBSCRIPTION_KEY_NAME, subscriptionKeyValue);
-
-            //create the payload
-            var payload = new
+            var credentials = new ApiKeyServiceClientCredentials(SubscriptionKey);
+            var client = new TextAnalyticsClient(credentials)
             {
-                documents = new[] { new Document() { language = "en", id = "1", text = documentText } ,
-                                    new Document() { language = "en", id = "2", text = "Everyone in my family liked the trail but thought it was too challenging for the less athletic among us. Not necessarily recommended for small children." }
-                }
+                Endpoint = Endpoint
             };
 
-            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            double sentimentResult = await SentimentAnalysisExample(client, documentText);
+            IList<string> keyPhraseResult = await KeyPhraseExtractionExample(client, documentText);
+
+            var returnCSValues = new CognitiveResult();
+
+            returnCSValues.Sentiment = sentimentResult;
+
+            if (returnCSValues.KeyPhrases == null)
             {
-                streamWriter.Write(JsonConvert.SerializeObject(payload, Formatting.Indented));
+                //It's null - create it
+                returnCSValues.KeyPhrases = new List<KeyPhrase>();
             }
 
-            //call the web service and get the response
-            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            foreach (var phrase in keyPhraseResult)
             {
-                result = streamReader.ReadToEnd();
+
+                returnCSValues.KeyPhrases.Add(new KeyPhrase { keyword = phrase });
+                
             }
 
-            return result;
+            // Not used:
+            // DetectLanguageExample(client).Wait();
+            // RecognizeEntitiesExample(client).Wait();
+
+
+            //TODO: build a return variable that contains both the sentiment and key phrases!!
+
+            return JsonConvert.SerializeObject(returnCSValues, Formatting.Indented);
+        }
+
+        private string callStoryAI(string keyword, double sentiment)
+        {
+            if (sentiment > 0.5)
+                {
+                return $"That sounds like a really cool {keyword}";
+                }
+            else
+            {
+                return $"I am sorry to hear about the {keyword}...";
+            }
         }
 
         class Document
@@ -173,6 +218,82 @@ namespace WrAppBot.Dialogs
 
         }
 
+        class CognitiveResult
+        {
+            [JsonProperty("keyphrases")]
+            public List<KeyPhrase> KeyPhrases { get; set; }
+
+            [JsonProperty("sentiment")]
+            public double Sentiment { get; set; }
+
+        }
+
+        class KeyPhrase
+        {
+            public string keyword;
+        }
+
+
+        /// <summary>
+        /// Allows authentication to the API by using a basic apiKey mechanism
+        /// </summary>
+        class ApiKeyServiceClientCredentials : ServiceClientCredentials
+        {
+            private readonly string subscriptionKey;
+
+            /// <summary>
+            /// Creates a new instance of the ApiKeyServiceClientCredentails class
+            /// </summary>
+            /// <param name="subscriptionKey">The subscription key to authenticate and authorize as</param>
+            public ApiKeyServiceClientCredentials(string subscriptionKey)
+            {
+                this.subscriptionKey = subscriptionKey;
+            }
+
+            /// <summary>
+            /// Add the Basic Authentication Header to each outgoing request
+            /// </summary>
+            /// <param name="request">The outgoing request</param>
+            /// <param name="cancellationToken">A token to cancel the operation</param>
+            public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                if (request == null)
+                {
+                    throw new ArgumentNullException("request");
+                }
+
+                request.Headers.Add("Ocp-Apim-Subscription-Key", this.subscriptionKey);
+                return base.ProcessHttpRequestAsync(request, cancellationToken);
+            }
+        }
+
+        public static async Task<double> SentimentAnalysisExample(TextAnalyticsClient client, string inputText)
+        {
+            // The documents to be analyzed. Add the language of the document. The ID can be any value.
+            var inputDocuments = new MultiLanguageBatchInput(
+                new List<MultiLanguageInput>
+                {
+                    new MultiLanguageInput("en", "1", inputText)
+                });
+            //...
+
+            var result = await client.SentimentAsync(false, inputDocuments);
+
+            return Convert.ToDouble(result.Documents[0].Score);
+        }
+
+        public static async Task<IList<string>> KeyPhraseExtractionExample(TextAnalyticsClient client, string inputText)
+        {
+            var inputDocuments = new MultiLanguageBatchInput(
+                        new List<MultiLanguageInput>
+                        {
+                            new MultiLanguageInput("en", "1", inputText),
+                        });
+            var kpResults = await client.KeyPhrasesAsync(false, inputDocuments);
+
+            return kpResults.Documents[0].KeyPhrases;
+
+        }
 
     }
 }
