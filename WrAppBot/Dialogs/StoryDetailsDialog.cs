e16c990fca9cee1bcb44348414b5005661d26def
trstroem <trstroem@microsoft.com>
2019-07-25 14:36:03 -0700

    corrected ortography

--- WrAppBot/Dialogs/StoryDetailsDialog.cs
+++ WrAppBot/Dialogs/StoryDetailsDialog.cs
@@ -113,14 +113,23 @@ namespace WrAppBot.Dialogs
 
             var sentiment = deserializedDocument.Sentiment;
             string resultFromStoryAI = "";
+            int numberOfCalls = 2;
 
             foreach (var keyphrase in deserializedDocument.KeyPhrases) 
                 {
                     resultFromStoryAI += callStoryAI(keyphrase.keyword.ToString(), sentiment);
+                    numberOfCalls -= 1;
+                    if (numberOfCalls == 0)
+                    {
+                        break;
+                    }
                 }
 
-//            var firstKeyword = deserializedDocument.KeyPhrases[0].keyword.ToString();
-//            var resultFromStoryAI = callStoryAI(firstKeyword, sentiment);
+            resultFromStoryAI += "!";
+
+
+            //            var firstKeyword = deserializedDocument.KeyPhrases[0].keyword.ToString();
+            //            var resultFromStoryAI = callStoryAI(firstKeyword, sentiment);
 
             var msg = $"{resultFromStoryAI}";
 
@@ -216,38 +225,38 @@ namespace WrAppBot.Dialogs
                 switch (phraseNumber)
             	{
                     case 1:
-                        phraseText = $"That sounds like a really cool {keyword}! What color is it?";
+                        phraseText = $" That sounds like a really cool {keyword}! What color is it?";
                         break;
                     case 2:
-                        phraseText = $"I once had a {keyword}, too! But then, someone stole it";
+                        phraseText = $" I once had a {keyword}, too! But then, someone stole it";
                         break;
                     case 3:
-                        phraseText = $"When I was younger, I wanted to be a {keyword}. They're so cool";
+                        phraseText = $" When I was younger, I wanted to be a {keyword}. They're so cool";
                         break;
                     case 4:
-                        phraseText = $"My uncle once trapped a {keyword} in his swimming pool. Can you believe it?";
+                        phraseText = $" My uncle once trapped a {keyword} in his swimming pool. Can you believe it?";
                         break;
                     case 5:
-                        phraseText = $"I like {keyword}'s. Especially the red ones";
+                        phraseText = $" I like {keyword}'s. Especially the red ones";
                         break;
                     case 6:
-                        phraseText = $"Can you tell me more about the {keyword}?";
+                        phraseText = $" Can you tell me more about the {keyword}?";
                         break;
                     case 7:
-                        phraseText = $"Wow! I never met anyone who talked about {keyword}!";
+                        phraseText = $" Wow! I never met anyone who talked about {keyword}!";
                         break;
                     case 8:
-                        phraseText = $"What did the {keyword} look like?";
+                        phraseText = $" What did the {keyword} look like?";
                         break;
                     case 9:
-                        phraseText = $"Was it a smelly {keyword}? I heard most of them are";
+                        phraseText = $" Was it a smelly {keyword}? I heard most of them are";
                         break;
                     case 10:
-                        phraseText = $"Can I play with your {keyword}? It's so boring to live inside a computer..";
+                        phraseText = $" Can I play with your {keyword}? It's so boring to live inside a computer..";
                         break;
 
                     default:
-                        phraseText = $"I really like what you tell me about the {keyword}";
+                        phraseText = $" I really like what you tell me about the {keyword}";
                         break;
 
 	            }
@@ -257,38 +266,38 @@ namespace WrAppBot.Dialogs
                 switch (phraseNumber)
             	{
                     case 1:
-                        phraseText = $"That sounds like a really horrible {keyword}";
+                        phraseText = $" That sounds like a really horrible {keyword}";
                         break;
                     case 2:
-                        phraseText = $"That sounds like a sad story. Tell me more about the {keyword}";
+                        phraseText = $" That sounds like a sad story. Tell me more about the {keyword}";
                         break;
                     case 3:
-                        phraseText = $"What did the {keyword} look like? Was it scary?";
+                        phraseText = $" What did the {keyword} look like? Was it scary?";
                         break;
                     case 4:
-                        phraseText = $"I know what happened next. The {keyword} freaked out and scared your cat, right?";
+                        phraseText = $" I know what happened next. The {keyword} freaked out and scared your cat, right?";
                         break;
                     case 5:
-                        phraseText = $"Hearing about {keyword} makes me sad. Can we talk about something else?";
+                        phraseText = $" Hearing about {keyword} makes me sad. Can we talk about something else?";
                         break;
                     case 6:
-                        phraseText = $"Are you really sure there was a {keyword}?";
+                        phraseText = $" Are you really sure there was a {keyword}?";
                         break;
                     case 7:
-                        phraseText = $"I can't believe it. A real {keyword}?";
+                        phraseText = $" I can't believe it. A real {keyword}?";
                         break;
                     case 8:
-                        phraseText = $"I don't really like the thing about the {keyword}";
+                        phraseText = $" I don't really like the thing about the {keyword}";
                         break;
                     case 9:
-                        phraseText = $"I'm sure the {keyword} was big and ugly, right?";
+                        phraseText = $" I'm sure the {keyword} was big and ugly, right?";
                         break;
                     case 10:
-                        phraseText = $"Once, an evil {keyword} stole my fake beard. You never knew chatbots had beards, did you?";
+                        phraseText = $" Once, an evil {keyword} stole my fake beard. You never knew chatbots had beards, did you?";
                         break;
 
                     default:
-                        phraseText = $"I'm really sad to hear about the horrible {keyword}. Can you tell me more?";
+                        phraseText = $" I'm really sad to hear about the horrible {keyword}. Can you tell me more?";
                         break;
 	            }
             }
